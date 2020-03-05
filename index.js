0d3f4cdcb744f4bd6f20f9c2fbb8a62f40e525b6
Anshuman Gupta <anshuman844@gmail.com>
2020-03-05 21:26:29 +0530

    Timestamp API created.   - The API endpoint is GET [project_url]/api/timestamp/:date_string   - date_string is valid if successfully parsed by new Date(date_string)   - If date_string is empty it trigger new Date(), current timestamp.   - The API return JSON, structure depends on validity of date_string.   - If valid, {"unix": <date.getTime()>, "utc" : <date.toUTCString()> }   - If not, {"error": "Invalid Date"}

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Hello from Node...")
+const express = require('express');
+const app = express();
+
+app.get('/api/timestamp', (req, res) => {
+  res.json({
+    unix: new Date().getTime(),
+    utc: new Date().toUTCString()
+  });
+});
+
+app.get('/api/timestamp/:date_string', (req, res) => {
+  const date_string = req.params.date_string;
+  // Check wheather the date_string consist of digits only.
+  if (/^\d*$/.test(date_string)) {
+    // Create a new Date object by casting date_string to Number from String (default parameter type).
+    const date = new Date(Number(date_string));
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+  else {
+    const date = new Date(date_string);
+    // Check if the date is a valid date object.
+    if (date instanceof Date && !isNaN(date)) {
+      res.json({
+        unix: date.getTime(),
+        utc: date.toUTCString()
+      });
+    }
+    else {
+      res.json({error: "Invalid Date"});
+    }
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on loclhost:3000");
+});
