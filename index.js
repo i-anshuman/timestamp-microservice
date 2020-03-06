d13d252fb98c6654f974f233167ae9a2c16fa191
Anshuman Gupta <anshuman844@gmail.com>
2020-03-06 13:46:46 +0530

    Middleware added to allow CORS requet.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 app.use(express.static(__dirname + "/assets/"));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
